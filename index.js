70efa50c82e858776aa1da03f83cefb2f05df27b
Devjiro76 <devjiro76@gmail.com>
2018-07-06 14:17:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ new Vue({
   el: "#app",
   template: `
     <div>
+      <h4>Open your console and select a image file</h4>
       <input type="file" @change="fileChange" accept="image/*" />
       <div>
         <pre v-html="ascii"></pre>
